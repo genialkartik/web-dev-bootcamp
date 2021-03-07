3405e8d16d760a76ada949c9fcb4d95626b81da2
Kartik Tyagi <kartik9756@gmail.com>
2021-03-07 19:18:23 +0530

    modified

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,53 @@
-
 import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActions, CardContent, Button, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
   return (
-    <div className="App">
-      <div>Kartik Tyagi</div>    
-    </div>
+    <>
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography className={classes.title} color="textSecondary" gutterBottom>
+            Word of the Day
+        </Typography>
+          <Typography variant="h5" component="h2">
+            be{bull}nev{bull}o{bull}lent
+        </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+            adjective
+        </Typography>
+          <Typography variant="body2" component="p">
+            well meaning and kindly.
+          <br />
+            {'"a benevolent smile"'}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </>
   );
 }
 
